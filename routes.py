c131b174660659dbeaf97269fb3138b1432b6d67
mirelle01 <lybackmirelle@gmail.com>
2022-09-28 12:48:47 +0300

    Uusi keskustelu -sivu

--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from app import app
 from flask import render_template, request, redirect
 import users
@@ -5,6 +6,7 @@ import users
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
@@ -33,6 +35,9 @@ def register():
         if len(username)<1 or len(username)>20:
             return render_template("error.html", message="Tunnuksessa tulee olla 1-20 merkkiä")
 
+        if " " in username:
+            return render_template("error.html", message="Tunnuksessa ei saa olla välilyöntejä")
+
         password1 = request.form["password1"]
         password2 = request.form["password2"]
 
@@ -42,6 +47,9 @@ def register():
         if password1 == "":
             return render_template("error.html", message="Salasana on tyhjä")
 
+        if " " in password1:
+            return render_template("error.html", message="Salasanassa ei saa olla välilyöntejä")
+
         role = request.form["role"]
         if role not in ("1", "2"):
             return render_template("error.html", message="Tuntematon käyttäjärooli")
@@ -49,3 +57,24 @@ def register():
         if not users.register(username, password1, role):
             return render_template("error.html", message="Rekisteröinti ei onnistunut")
         return redirect("/")
+
+@app.route("/new", methods=["GET", "POST"])
+def new_title():
+    if request.method =="GET":
+        return render_template("new.html")
+
+    if request.method == "POST":
+        return redirect("/")
+
+@app.route("/create", methods=["GET", "POST"])
+def created():
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        title = request.form["title"]
+        comment = request.form["comment"]
+        if title == "":
+            return render_template("error.html", message="Otsikko ei saa olla tyhjä!")
+        if comment == "" or  comment == "    ":
+            return render_template("error.html", message="Aloituskommentti ei saa olla tyhjä!")
+        return render_template("create.html")
